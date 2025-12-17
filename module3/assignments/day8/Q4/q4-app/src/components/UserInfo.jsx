a77195339f861555bc56b1e9357eda8d94734ccf
kusumagurrala <kusuma.bts@gmail.com>
2025-12-17 21:48:53 +0530

    Added UserProfile and UserInfo components with props

---
+++ module3/assignments/day8/Q4/q4-app/src/components/UserInfo.jsx
@@ -0,0 +1,11 @@
+function UserInfo({ name, age }) {
+    return (
+      <div>
+        <p><strong>Name:</strong> {name}</p>
+        <p><strong>Age:</strong> {age}</p>
+      </div>
+    );
+  }
+  
+  export default UserInfo;
+  
