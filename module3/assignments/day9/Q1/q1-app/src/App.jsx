24ffab5eaf103c8c355b5ccf25ff3a58dbc58916
kusumagurrala <kusuma.bts@gmail.com>
2025-12-19 17:02:17 +0530

    Added day9

---
+++ module3/assignments/day9/Q1/q1-app/src/App.jsx
@@ -0,0 +1,12 @@
+import UserData from "./components/UserData";
+
+function App() {
+  return (
+    <div>
+      <h2>Fetch User Data</h2>
+      <UserData />
+    </div>
+  );
+}
+
+export default App;
