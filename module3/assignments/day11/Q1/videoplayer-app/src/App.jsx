b3383266d9aa4979e14a9abbfa70f90a76158ee2
kusumagurrala <kusuma.bts@gmail.com>
2025-12-22 22:34:03 +0530

    Added day11

---
+++ module3/assignments/day11/Q1/videoplayer-app/src/App.jsx
@@ -0,0 +1,12 @@
+import VideoPlayer from "./components/VideoPlayer";
+
+function App() {
+  return (
+    <div>
+      <h2>Video Player using useRef</h2>
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
