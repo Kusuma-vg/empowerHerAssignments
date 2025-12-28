65ece173086a27c74eaa46157e393eff8ddb7be9
kusumagurrala <kusuma.bts@gmail.com>
2025-12-28 19:26:54 +0530

    Added Protected jsx and Login.jsx

--- MockEvaluations/module3mock2/fleetmanagement-app/src/pages/Login.jsx
+++ MockEvaluations/module3mock2/fleetmanagement-app/src/pages/Login.jsx
@@ -1,1 +1,29 @@
-import 
+import React,{useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+const Login=({setIsAuth})=>{
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const navigate=useNavigate();
+    const handleLogin=(e)=>{
+        e.preventDefault();
+        if(email === "admin@gmail.com" && password === "admin123"){
+            setIsAuth(true);
+            navigate("/dashboard");
+        }
+        else{
+            alert("Invalid crendentials");
+        }
+    };
+    return (
+        <div sytle={{padding:"40px"}}>
+            <h2>Admin Login</h2>
+            <form onSubmit={handleLogin}>
+                <input type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} required />
+                <br /><br />
+                <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} required />
+                <br /><br />
+            </form>
+        </div>
+    );
+}
